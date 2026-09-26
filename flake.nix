bb94b3ba2b82b6d93fddb32ee009fe8ca4d46840
Trilowy <49493635+trilowy@users.noreply.github.com>
2026-09-26 19:27:46 +0200

    chore: stable Nix version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 {
   description = "Zig project";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs = { nixpkgs, ... }:
   let
